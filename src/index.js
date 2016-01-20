db056b40ccb6b834166985488cebaff7aa8de003
jankuca <jan@jankuca.com>
2016-01-20 03:52:46 +0100

    add index

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Component from './component'
+import DomDriver from './dom-driver'
+import Renderer from './renderer'
+import element from './element'
+
+
+export {
+  Component,
+  DomDriver,
+  Renderer,
+  element
+}
